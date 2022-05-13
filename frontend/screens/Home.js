4edadb3d968df89ff5b3d5715e8df5257885274a
BeniDiegoDev <benjamin.donofrio@outlook.fr>
2022-05-13 12:17:25 +0200

    suppression safeareaview pour compatibilité entre ios et android

--- frontend/screens/Home.js
+++ frontend/screens/Home.js
@@ -40,104 +40,102 @@ function Home() {
   ]
 
   return (
-    <SafeAreaView style={styles.safearea}>
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.container}>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.container}>
 
-          <View style={styles.finded}>
-            <Text style={{
-              color: '#7241DB',
-              fontWeight: 'bold',
-              fontStyle: 'italic',
-              textAlign: 'center',
-              fontSize: 20,
-            }}>Finded</Text>
+        <View style={styles.finded}>
+          <Text style={{
+            color: '#7241DB',
+            fontWeight: 'bold',
+            fontStyle: 'italic',
+            textAlign: 'center',
+            fontSize: 20,
+          }}>Finded</Text>
+        </View>
+
+        <View style={styles.topsearchbar}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 10 }}>
+            <Ionicons name='location' size={32} color='#3DA787' />
+            <Text style={{ fontWeight: 'bold', marginLeft: 10, fontSize: 17 }}>Paris 17e</Text>
           </View>
-
-          <View style={styles.topsearchbar}>
-            <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 10 }}>
-              <Ionicons name='location' size={32} color='#3DA787' />
-              <Text style={{ fontWeight: 'bold', marginLeft: 10, fontSize: 17 }}>Paris 17e</Text>
-            </View>
-            <View style={{ marginRight: 10 }}>
-              <Button
-                buttonStyle={{ borderColor: "#7241DB" }}
-                titleStyle={{ color: '#7241DB', fontSize: 17 }}
-                title="Autour de vous"
-                type="outline"
-                containerStyle={{ marginLeft: 20, }}
-              />
-            </View>
-          </View>
-
-          <View style={styles.searchbar}>
-            <SearchBar
-              placeholder="Recherche"
-              onChangeText={updateSearch}
-              value={search}
-              lightTheme="true"
-              containerStyle={{ backgroundColor: 'white', borderTopColor: 'white', borderBottomColor: 'white' }}
-              leftIconContainerStyle={{ backgroundColor: 'white' }}
-              inputStyle={{ backgroundColor: 'white' }}
-              inputContainerStyle={{ backgroundColor: 'white' }}
+          <View style={{ marginRight: 10 }}>
+            <Button
+              buttonStyle={{ borderColor: "#7241DB" }}
+              titleStyle={{ color: '#7241DB', fontSize: 17 }}
+              title="Autour de vous"
+              type="outline"
+              containerStyle={{ marginLeft: 20, }}
             />
           </View>
+        </View>
 
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-            <View style={styles.categories}>
-              {fakeCategories.map((element, i) => {
-                return (
-                  <View style={styles.categorieswidget}>
-                    <Avatar
-                      rounded
-                      icon={element.image}
-                      containerStyle={{ backgroundColor: 'orange' }}
-                      size="large"
-                    />
-                    <Text style={{ textAlign: 'center' }}>{element.name}</Text>
-                  </View>
-                )
-              })
-              }
-            </View>
-          </ScrollView>
+        <View style={styles.searchbar}>
+          <SearchBar
+            placeholder="Recherche"
+            onChangeText={updateSearch}
+            value={search}
+            lightTheme="true"
+            containerStyle={{ backgroundColor: 'white', borderTopColor: 'white', borderBottomColor: 'white' }}
+            leftIconContainerStyle={{ backgroundColor: 'white' }}
+            inputStyle={{ backgroundColor: 'white' }}
+            inputContainerStyle={{ backgroundColor: 'white' }}
+          />
+        </View>
 
-          <View style={styles.categoriestext}>
-            <Text style={{ marginRight: 5, fontWeight: 'bold' }}>Voir tout</Text>
-            <Ionicons name='chevron-forward' size={15} color='black' />
-          </View>
-
-          <View style={styles.cards}>
-            {fakeTableau.map((element, i) => {
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <View style={styles.categories}>
+            {fakeCategories.map((element, i) => {
               return (
-                <Card>
-                  <View style={{ flexDirection: 'row' }}>
-                    <Avatar
-                      rounded
-                      icon={element.image}
-                      containerStyle={{ backgroundColor: 'orange' }}
-                      size="large"
-                    />
-                    <View style={{ marginLeft: 15 }}>
-                      <Text>{element.name}</Text>
-                      <Text>{element.city}</Text>
-                      <Text style={{maxWidth : '90%'}}>{element.desc}</Text>
-                      <View>
-                        <Text>{element.note}</Text>
-                      </View>
-                    </View>
-                  </View>
-                </Card>
+                <View style={styles.categorieswidget}>
+                  <Avatar
+                    rounded
+                    icon={element.image}
+                    containerStyle={{ backgroundColor: 'orange' }}
+                    size="large"
+                  />
+                  <Text style={{ textAlign: 'center' }}>{element.name}</Text>
+                </View>
               )
             })
             }
           </View>
+        </ScrollView>
 
+        <View style={styles.categoriestext}>
+          <Text style={{ marginRight: 5, fontWeight: 'bold' }}>Voir tout</Text>
+          <Ionicons name='chevron-forward' size={15} color='black' />
         </View>
 
-      </ScrollView>
-    </SafeAreaView>
+        <View style={styles.cards}>
+          {fakeTableau.map((element, i) => {
+            return (
+              <Card>
+                <View style={{ flexDirection: 'row' }}>
+                  <Avatar
+                    rounded
+                    icon={element.image}
+                    containerStyle={{ backgroundColor: 'orange' }}
+                    size="large"
+                  />
+                  <View style={{ marginLeft: 15 }}>
+                    <Text>{element.name}</Text>
+                    <Text>{element.city}</Text>
+                    <Text style={{ maxWidth: '90%' }}>{element.desc}</Text>
+                    <View>
+                      <Text>{element.note}</Text>
+                    </View>
+                  </View>
+                </View>
+              </Card>
+            )
+          })
+          }
+        </View>
+
+      </View>
+
+    </ScrollView>
   );
 }
 
@@ -149,6 +147,8 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
     alignItems: 'center',
+    flex: 1,
+    paddingTop: 40,
   },
   finded: {
     width: '100%',
