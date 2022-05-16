e85336da6b75c255d390ade530005dab5c4fb5e7
BeniDiegoDev <benjamin.donofrio@outlook.fr>
2022-05-16 17:16:06 +0200

    refonte de la connexion.js

--- backend/models/connexion.js
+++ backend/models/connexion.js
@@ -2,14 +2,18 @@ var mongoose = require('mongoose');
 
 var options = {
     connectTimeoutMS: 5000,
-    useUnifiedTopology : true,
+    useUnifiedTopology: true,
     useNewUrlParser: true,
 }
 
 mongoose.connect('mongodb+srv://benidiegodev:benibenibeni@benidiegodev.youor.mongodb.net/findedApp?retryWrites=true&w=majority',
     options,
-    function(err){
-        console.log(err);
+    function (err) {
+        if (err) {
+            console.error('Erreur de connexion à mongoDB : ' + err);
+        } else {
+            console.log('connecté à la DB')
+        }
     }
 )
 
