3e8b7bfbb056afa99458a6aa63ecbac6383298ee
Léo Baccialone <leo.baccialone@laposte.net>
2022-05-17 12:22:44 +0200

    Ajout Mon Wallet

--- frontend/screens/ProfilPages/Adresses.js
+++ frontend/screens/ProfilPages/Adresses.js
@@ -1,82 +1,85 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-import { FontAwesome } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 
+import Swipeout from 'react-native-swipeout';
+
 export default function Adresses(props) {
+
+  var swipeoutBtns = [
+    {
+      text: 'Supprimer',
+      backgroundColor: '#FF0000',
+    },
+    {
+      text: 'Modifier',
+      backgroundColor: '#7241DB',
+    }
+  ]
+
+  let adresses = [
+    {
+      icon : <MaterialCommunityIcons name="home-variant" size={35} color="#7241DB" style={{marginRight:10}}/>,
+      name : 'Domicile',
+      adress : '12 Rue de la paix\n75000 Paris',
+    },
+    {
+      icon : <MaterialIcons name="card-travel" size={35} color="#7241DB" style={{marginRight:10}}/>,
+      name : 'Travail',
+      adress : '14 Avenue Mongo\n33000 Bordeaux',
+    },
+    {
+      icon : <Entypo name="feather" size={30} color="#7241DB" style={{marginRight:10}}/> ,
+      name : 'Vacances',
+      adress : `1 Boulevard des vacances\n06200 Nice`,
+    }
+  ];
+
+  let adressesList = adresses.map((item, index) => {
+    return (
+      <Swipeout autoClose right={swipeoutBtns} style={{backgroundColor:'white'}}>
+        <View style={styles.personnal_informations} key={index}>
+          <View style={styles.adresses_container}>
+              {item.icon}
+              <View>
+                <Text style={styles.icon}>{item.name}</Text>
+                <Text style={[styles.icon, styles.adresses]}>{item.adress}</Text>
+              </View>
+          </View>
+        </View>
+      </Swipeout>
+    )
+  })
 
  
     return (
-        <View style={styles.container}>
-          <View>
-            <View style={{marginVertical:40, display:'flex', flexDirection:'row', borderBottomWidth: 1, paddingBottom:30}}>
-                <Text style={{fontSize:30}}><Ionicons name="chevron-back" size={30} color="black" onPress={() => { props.navigation.goBack(null)}}/> Mes adresses</Text>
-            </View>
-            
-            <View style={styles.personnal_informations}>
-              <View style={styles.adresses_container}>
-                  <MaterialCommunityIcons name="home-variant" size={35} color="black" style={{marginRight:10}}/>
-                  <View>
-                    <Text style={styles.icon}>Domicile</Text>
-                    <Text style={[styles.icon, styles.adresses]}>21 rue de Finded - 75017</Text>
-                  </View>
-              </View>
-              <View>
-                <Text style={styles.icon}><FontAwesome name="pencil" size={20} color="black" /></Text>
-                <Text style={styles.icon}><Ionicons name="trash" size={20} color="black" /></Text>
-              </View>
-            </View>
-
-            <View style={styles.personnal_informations}>
-              <View style={styles.adresses_container}>
-              <MaterialIcons name="card-travel" size={35} color="black" style={{marginRight:10}}/>
-                  <View>
-                    <Text style={styles.icon}>Travail</Text>
-                    <Text style={[styles.icon, styles.adresses]}>14 avenue Mongo - 92100</Text>
-                  </View>
-              </View>
-              <View>
-                <Text style={styles.icon}><FontAwesome name="pencil" size={20} color="black" /></Text>
-                <Text style={styles.icon}><Ionicons name="trash" size={20} color="black" /></Text>
-              </View>
-            </View>
-
-            <View style={styles.personnal_informations}>
-              <View style={styles.adresses_container}>
-              <Entypo name="feather" size={30} color="black" style={{marginRight:10}}/>
-                  <View>
-                    <Text style={styles.icon}>Vacances</Text>
-                    <Text style={[styles.icon, styles.adresses]}>3 rue des vacances - 06200</Text>
-                  </View>
-              </View>
-              <View>
-                <Text style={styles.icon}><FontAwesome name="pencil" size={20} color="black" /></Text>
-                <Text style={styles.icon}><Ionicons name="trash" size={20} color="black" /></Text>
-              </View>
-            </View>
-          </View>
-
-            <View style={{display:'flex', alignItems:'center'}}>
-              <Text style={{marginBottom:20, color: '#7241DB', fontWeight:'bold', fontSize:15}}>Ajouter une adresse</Text>
-            </View>
+      <View style={styles.container}>
+      <View>
+        <View style={{display:'flex', flexDirection:'row', paddingBottom:30}}>
+            <Text style={{fontSize:30, marginHorizontal:10}}><Ionicons name="chevron-back" size={30} color="black" onPress={() => { props.navigation.goBack(null)}}/> Mes adresses</Text>
         </View>
+            {adressesList}
+      </View>
+        <View style={{display:'flex', alignItems:'center'}}>
+          <Text style={{marginBottom:20, color: '#7241DB', fontWeight:'bold', fontSize:15}}>Ajouter une adresse</Text>
+        </View>
+    </View>
     );
   
   };
 
 const styles = StyleSheet.create({
     container:{
-        paddingTop:10,
-        flex:1,
-        backgroundColor:'#fff',
-        paddingHorizontal:10,
-        display:'flex',
-        flexDirection:'column',
-        justifyContent:'space-between'
+      paddingTop:50,
+      flex:1,
+      backgroundColor:'#fff',
+      display:'flex',
+      flexDirection:'column',
+      justifyContent:'space-between'
     },
     icon: {
         marginHorizontal:10,
@@ -87,15 +90,8 @@ const styles = StyleSheet.create({
         display:'flex',
         flexDirection:'row',
         justifyContent:'space-between',
-        paddingVertical:25,
-        paddingHorizontal:10
-    },
-    modify_remove: {
-        display:'flex',
-        flexDirection:'row',
-        justifyContent:'center',
-        borderBottomWidth: 1,
-        paddingVertical:30
+        paddingVertical:10,
+        marginHorizontal:20,  
     },
     button: {
         marginTop:30,
