67e124f2dd7916abd09d860e83ee758e73b4dc3a
Léo Baccialone <leo.baccialone@laposte.net>
2022-05-18 10:48:38 +0200

    ajout favoris

--- frontend/screens/ProfilPages/Favoris.js
+++ frontend/screens/ProfilPages/Favoris.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { ListItem, Avatar, Button } from 'react-native-elements';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
-
-import { AntDesign } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 
-export default function Favoris() {
+import Swipeout from 'react-native-swipeout';
 
+// Import components
+import Listing from '../../components/Listing'
+
+// Import de la connexion avec Redux
+import { connect } from 'react-redux'
+
+function Favoris(props) {
+
+
+  let listingFilter = props.preStataires.filter(elem => elem.note >= 1)
+
+  let listing = listingFilter.map((element, i) => {
+    return (
+        <Listing key={i} navigation={props.navigation} name={element.name} number={element.number} images={element.images} address={element.address} zipcode={element.zipcode} city={element.city} note={element.note} />
+    )
+  })
  
     return (
         <View style={styles.container}>
-          <Text>Favoris</Text> 
+            <View style={{display:'flex', flexDirection:'row', paddingBottom:30}}>
+                <Text style={{fontSize:30, marginHorizontal:10}}><Ionicons name="chevron-back" size={30} color="black" onPress={() => { props.navigation.goBack(null)}}/> Mes Favoris</Text>
+            </View>
+            <ScrollView style={{ width: '100%' }} showsVerticalScrollIndicator={false} >
+              {listing}
+            </ScrollView >
         </View>
     );
   
@@ -20,11 +38,60 @@ export default function Favoris() {
 
 const styles = StyleSheet.create({
     container:{
-        paddingTop:40,
-        flex:1,
-        backgroundColor:'#fff',
+      paddingTop:50,
+      flex:1,
+      backgroundColor:'#fff',
+      display:'flex',
+      flexDirection:'column',
+      justifyContent:'space-between'
+    },
+    icon: {
+        paddingHorizontal:10,
+        marginVertical: 5, 
+        fontSize: 18
+    },
+    personnal_informations: {
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        paddingVertical:25,
         paddingHorizontal:20
+    },
+    modify_remove: {
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'center',
+        borderBottomWidth: 1,
+        paddingVertical:30
+    },
+    button: {
+        marginTop:30,
+    },
+    adresses_container: {
+        display:'flex',
+        flexDirection:'row',
+        alignItems:'center',
+    },
+    adresses: {
+        color:'#828282'
+    },
+    cards_infos: {
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'space-between',
+        alignItems:'space-between',
     }
+
 });
+
+function mapStateToProps(state) {
+  return { preStataires: state.prestataires, }
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(Favoris);
+
 
   
