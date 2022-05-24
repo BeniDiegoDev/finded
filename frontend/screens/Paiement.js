4edfe5fcc8ae3c4af0fa3db8b1a71e79789e8c1d
stonoli <stonolidm@gmail.com>
2022-05-24 10:49:10 +0200

    my IP

--- frontend/screens/Paiement.js
+++ frontend/screens/Paiement.js
@@ -15,7 +15,7 @@ import { Entypo } from '@expo/vector-icons';
 
 import LottieView from 'lottie-react-native';
 
-const ip = "192.168.10.128";
+const ip = "192.168.0.25";
 
 
 import { Divider, Tab } from 'react-native-elements';
